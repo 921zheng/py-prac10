4656064bf6c2a9f0dd808fcf67ddd72d6778dfd1
Sophie Zheng <139349595+921zheng@users.noreply.github.com>
2023-12-15 16:31:19 +0000

    Add files via upload

---
+++ py-prac.py
@@ -0,0 +1,75 @@
+# 1.	Write a Python program to check if the given number is palindrome or not.
+
+# a=12321
+# def check(n):
+#     b=str(n)
+#     if b[::-1]==b:
+#         return True
+#     else:
+#         return False
+# print(check(a))
+
+
+
+# 3.	Write a program to find a factorial of a number.
+
+# input_set = {1, 2, 3, 4, 5}
+# fact = 1
+# for item in input_set:
+#     for number in range(1,item+1):
+#         fact = fact * number
+#     print (fact)
+
+# 5.	Write a Python program to find a Fibonacci of a number.
+
+    
+
+
+
+# a = int(input('Give amount: '))
+
+# def fib(n):
+#     a, b = 0, 1
+#     for _ in range(n):
+#         yield a
+#         a, b = b, a + b
+
+# print(list(fib(a)))
+
+# 6.	Write a Python program to print the following pattern.
+
+# def check(n):
+#     for i in range(1,n+1):
+#         for j in range(1,i+1):
+#           print(j, end=' ')
+#         print()
+# check(5)
+
+# for i in range(1, 6):
+#     for j in range(1, i+1):
+#         print(j, end=' ')
+#     print()
+# # 8.	Write a Python program to print in the xmas tree pattern.
+
+# def tree(n):
+#     for i in range(n):
+#         print(("*" * (i +1)).center(n * 2 - 1))
+
+# tree(5)
+
+
+
+# 9.	Create a Python program that checks if a given sequence is a Palindrome.
+
+# string = "abcba"
+# revstring = "".join(reversed(string))
+
+# print("Yes" if string == revstring else "No")
+
+
+
+
+
+
+
+  
